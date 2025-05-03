73e221b2d1d8dd45c5e58101d4239d2ef9633c8a
ox-sander-xo <119248842+ox-sander-xo@users.noreply.github.com>
2025-05-03 17:44:54 -0400

    Commiting completed exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+ const letters = str.split("");
+ console.log(letters);
+ let reversedArray = [];
+  for (let i = letters.length; i > 0; i--) {
+  const popped = letters.pop();
+  reversedArray.push(popped)
+  console.log(reversedArray)
+ }
+ return reversedArray.join("")
+ console.log(reversedArray)
 };
 
-// Do not edit below this line
+// Do not edit below this line˜˜
 module.exports = reverseString;
