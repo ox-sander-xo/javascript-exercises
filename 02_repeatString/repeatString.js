c31eb24a40712aa96f9ae55b3f19600e37d6e500
ox-sander-xo <119248842+ox-sander-xo@users.noreply.github.com>
2025-05-03 16:46:56 -0400

    Commiting repeatString exercise with all tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+let newStr = '';
+if (num < 0) return 'ERROR'
+else{
+for (let i = 1; i <= num; i++) {
+  newStr += str;
+}
+ console.log(newStr)
+ return newStr
+ }
 };
-
+console.log(repeatString('hey', 0))
 // Do not edit below this line
 module.exports = repeatString;
