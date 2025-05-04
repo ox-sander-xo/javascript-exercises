18abbcfde60ddf86c2e432f11729275164ef4200
ox-sander-xo <119248842+ox-sander-xo@users.noreply.github.com>
2025-05-04 17:05:58 -0400

    Committing completed challange

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
+    if (num1 < 0 || num2 < 0) return 'ERROR'
+    if (isNaN(num1 || num2)) return 'ERROR'
+    if (num1 % 1 !== 0 || num2 % 1 !== 0) return 'ERROR'
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR'
 
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++){
+                result += i
+            }
+        } else {
+            for (let i = num2; i <= num1; i++) {
+                result += i
+            }
+         }
+    return result
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
