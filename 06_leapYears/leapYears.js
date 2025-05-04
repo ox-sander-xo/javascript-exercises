517a57e5d79393acfc21a86182c0cec5beb37e99
ox-sander-xo <119248842+ox-sander-xo@users.noreply.github.com>
2025-05-04 17:46:38 -0400

    Commiting completed excersice. I like my solution better than the one provided.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+let result = true
+    if (year % 4 === 0 && year % 100 === 0) {
+    result = year % 400 === 0
+   } 
+    else {
+    result = year % 4 === 0 
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
