48c001f48a12970fab5d84a1dde81e1a3c9425e5
ox-sander-xo <119248842+ox-sander-xo@users.noreply.github.com>
2025-05-15 11:46:36 -0400

    commiting solutions to calculator problems.
    feeling proud of myself for sticking with it and finding solutions on my own.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let result = 0
+	for (let i = 0; i < args.length; i++) {
+   result += args[i]
+  //  console.log(result)
+  }
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+   let result = 1;
+   for (let i = 0; i < args.length; i++) {
+    result *= args[i]
+   }
+   return result
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let result = 1;
+  for (let i = 0; i < pow; i++) {
+   result *= num
+   console.log(result)
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+let total = num;
+
+if (num === 0) {
+  total = 1
+} else {
+for (let i = num - 1; i > 0; i--){
+  total = total * i
+ }
+}
+return total
 };
 
 // Do not edit below this line
